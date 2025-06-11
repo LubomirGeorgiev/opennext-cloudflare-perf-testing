314122480baacc042b7f9e5de2a76e65a9faf8f0
Lubomir Georgiev <lubomir.georgiev.georgiev@gmail.com>
2025-06-11 17:26:35 +0300

    Finalize the perf test results

--- v0-5/open-next.config.ts
+++ v0-5/open-next.config.ts
@@ -1,9 +1,12 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
+import memoryQueue from "@opennextjs/cloudflare/memory-queue";
+
 export default defineCloudflareConfig({
   // Uncomment to enable R2 cache,
   // It should be imported as:
   // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
   // See https://opennext.js.org/cloudflare/caching for more details
   // incrementalCache: r2IncrementalCache,
+  queue: memoryQueue,
 });
